be021fe0f212b4327c168c0da37b6ceda509da14
Adam Groom <adam.groom@gmail.com>
2017-11-26 01:56:56 +0100

    html added

--- app/views.py
+++ app/views.py
@@ -8,6 +8,18 @@ from app import app
 from api import get_trajectory_array
 
 
-@app.route('/parts')
+@app.route('/')
+def view_all_parts():
+    return render_template('index.html', info=flagged_wheels())
+
+@app.route('/wheels')
+def view_all_parts():
+    return render_template('wheels.html')    
+
+@app.route('/trajectory')
 def view_all_parts():
     return render_template('trajectory.html', info=get_trajectory_array())
+
+@app.route('/schedule')
+def view_all_parts():
+    return render_template('schedule.html', info=get_all_flagged_data())    
